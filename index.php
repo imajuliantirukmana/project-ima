f051379224534d44017be281b8391f552057d813
imajuliantirukmana <imajuliantir@gmail.com>
2019-04-26 08:59:24 +0700

    project ima

---
+++ index.php
@@ -0,0 +1,152 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <style type="text/css">
+      .body {
+          /*background-image: url("gambar/1.jpg");
+          height: 1000px;
+          background-position: center;
+          background-repeat: no-repeat;
+          background-size: cover;*/
+          background-color: #dfdfe1;
+      }
+      .img{
+        width: 30px;
+        height: 30px;
+      }
+      .navbar{
+        background-color: #b7b7eb;
+      }
+      h5{
+        color: white;
+      }
+      h2{
+        font-family: Superfats;
+        color: #999;
+      }
+      .nav-item{
+        align-items: left;
+      }
+      .container{
+        /*background-image: url("gambar/2.jpg");
+          height: 100%;
+          background-position: center;
+          background-repeat: no-repeat;
+          background-size: cover;*/
+          /*background-color: #582beb;*/
+          background-color: #dfdfe1;
+          /*opacity: 0.8;  */    
+        }
+      p{
+        color: black;
+        text-align: justify;
+      }
+    </style>
+  </head>
+  <body class="body">
+
+<nav class="navbar navbar-expand-lg navbar-light">
+  <a class="navbar-brand" href="#"><img src="gambar/orofi.png" width="80" height="43"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <h5>Jl. Lembah Pakar Timur No.99 Ciburial<br>Cimenyan, Bandung</h5>
+    <div class="navbar-nav offset-md-7">
+      <a class="nav-item nav-link " href="#"><img class="img" src="gambar/insta.png"></a>
+      <a class="nav-item nav-link" href="#"><img class="img" src="gambar/phone.png"></a>
+      <a class="nav-item nav-link" href="#"><img class="img" src="gambar/2.png"></a>
+    </div>
+  </div>
+</nav>
+
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #dfdfe1;">
+  <a class="navbar-brand" href="#" style="color: #fff; font-family: superfats;">OROFI</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Menu<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Booking<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Info<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Bantuan<span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="jumbotron jumbotron-fluid" style="background-color: #fff;">
+  <div class="container">
+    <br><br><br>
+    <div class='row'>
+      <div class='col-sm-5 offset-md-1'>
+        <h2><b>Orofi Cafe by The Valley</b></h2>
+        <p>Tempatnya terbagi jadi 3 pilihan yaitu area outdoor yang paling hits di instagram, area semi indoor yang ada atapnya, juga bar area. Pas kami datang lagi hujan jadi kita duduk di bar area.</p>
+      </div>
+      <div class='col-sm-5'>
+        <p>PT. Mandarin Sanghay Food (Shio)
+(Resto Gardensea Mandarin & Sanghay mandarin)
+Persyaratan :
+
+- Pria/Wanita Usia 18-35 thn
+
+- Bertanggung jawab terhadap pekerjaan
+
+- Pendidikan Min. SMP/SMA
+
+- Rapi, jujur, bersih, sopan, supel
+
+- Bisa berkomunikasi dengan baik
+
+- Dapat bekerja dengan team/individu
+
+- Disiplin & Loyal
+
+Persyaratan Umum :
+
+1. Surat lamaran/Cv
+
+2. F/C Ijazah
+
+3. F/C KTP/KK
+
+4. Pas Foto 2 lembar ukuran 3x4
+
+5. Berkas pendukung lain
+</p>
+    </div>
+  </div>
+
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+</div>
+
+<div class="jumbotron jumbotron-fluid">
+</div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
